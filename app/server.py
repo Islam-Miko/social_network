4aa3f4f656dd21da2f626090b93f7e55ca320737
miko <islamimanhod@gmail.com>
2023-01-10 21:55:00 +0600

    feat: Configure pre-commit-hooks

--- app/server.py
+++ app/server.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI, Request
 
-
 app = FastAPI()
 
 
 @app.get("/")
 async def starter(request: Request):
-    return {
-        "msg": "ok"
-    }
+    return {"msg": "ok"}
