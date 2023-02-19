39bb571a3fb3b673b4f4cd4b60ebf65fe5f9fd5b
miko <islamimanhod@gmail.com>
2023-02-19 16:23:14 +0600

    fix: move get_sesion to configs.database

--- app/configs/database.py
+++ app/configs/database.py
@@ -1,3 +1,5 @@
+from typing import AsyncIterator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -25,3 +27,11 @@ SessionFactory = sessionmaker(
     autoflush=False,
     expire_on_commit=False,
 )
+
+
+async def get_session() -> AsyncIterator[AsyncSession]:
+    session = SessionFactory()
+    try:
+        yield session
+    finally:
+        await session.close()
