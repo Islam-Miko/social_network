abf185f17d5c8b0fe48806960cb630eee95062ef
miko <islamimanhod@gmail.com>
2023-02-19 14:53:15 +0600

    fix: redefine base repositories encapsulated attr to public attr

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -1,11 +1,9 @@
 from collections.abc import Iterable, Mapping, Sequence
-from datetime import datetime
-from typing import Any, ClassVar, TypeVar
+from typing import Any, ClassVar, TypeVar, Union
 
 from fastapi import Depends
-from sqlalchemy import update
-from sqlalchemy.engine import Result, ResultProxy
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import delete, update
+from sqlalchemy.ext.asyncio import AsyncResult, AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql import exists as origin_exists
@@ -21,36 +19,25 @@ class BaseSqlAlchemyRepository:
     Class for working with db
     """
 
-    __session: AsyncSession
+    session: AsyncSession
     model: ClassVar
 
     def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
-        self.__session = session
+        self.session = session
 
     async def create(self, instance: Model) -> Model:
-        self.__session.add(instance)
-        await self.__session.flush()
+        self.session.add(instance)
+        await self.session.flush()
+        await self.session.commit()
         return instance
 
     async def get(self, id: Key) -> Model:
         try:
             query = select(self.model).where(id == id)
-            result: ResultProxy = await self.__session.execute(query)
+            result: AsyncResult = await self.session.execute(query)
             return result.scalars().one()
         except NoResultFound:
             raise  # TODO handle
-
-    async def delete(self, id: Key) -> None:
-        try:
-            query = (
-                update(self.model)
-                .where(self.model.id == id)  # type: ignore
-                .values(deleted_at=datetime.now())
-            )
-            result: ResultProxy = await self.__session.execute(query)
-            return result.one_or_none()
-        except NoResultFound:
-            raise
 
     async def update(self, id: Key, data: Mapping[str, Any]) -> Model:
         try:
@@ -60,7 +47,8 @@ class BaseSqlAlchemyRepository:
                 .returning(self.model)
                 .values(**data)
             )
-            result: ResultProxy = await self.__session.execute(query)
+            result: AsyncResult = await self.session.execute(query)
+            await self.session.commit()
             return result.first()
         except NoResultFound:
             raise
@@ -72,10 +60,20 @@ class BaseSqlAlchemyRepository:
         if args:
             query.filter(*args)
         query = query.limit(limit).offset(offset)
-        result: ResultProxy = await self.__session.execute(query)
+        result: AsyncResult = await self.session.execute(query)
         return result.scalars().all()
 
     async def exists(self, *args: Iterable) -> bool:
         query = origin_exists(self.model).where(*args).select()
-        result: Result = await self.__session.execute(query)
+        result: AsyncResult = await self.session.execute(query)
         return result.scalar_one()
+
+    async def get_or_none(self, *args: Iterable) -> Union[None, Model]:
+        query = select(self.model).filter(*args)
+        result: AsyncResult = await self.session.execute(query)
+        return result.scalars().first()
+
+    async def delete(self, id: Key) -> None:
+        query = delete(self.model).where(self.model.id == id)
+        await self.session.execute(query)
+        await self.session.commit()
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,6 +1,56 @@
-from app.models.user_model import User
+from datetime import date
+from typing import Optional
+
+from sqlalchemy import or_, select
+from sqlalchemy.ext.asyncio import AsyncResult
+
+from app.models.user_model import Credential, RefreshToken, User
 from app.repositories.base_repository import BaseSqlAlchemyRepository
 
 
 class UserRepository(BaseSqlAlchemyRepository):
     model = User
+
+    async def search(
+        self,
+        first_name: Optional[str],
+        last_name: Optional[str],
+        substr: Optional[str],
+        birth_date: Optional[date],
+    ) -> list[User]:
+        query = select(self.model).where(self.model.deleted_at.is_(None))
+
+        if first_name:
+            query = query.where(self.model.first_name == first_name)
+
+        if last_name:
+            query = query.where(self.model.last_name == last_name)
+
+        if substr:
+            query = query.where(
+                or_(
+                    User.last_name.icontains(substr),
+                    User.first_name.icontains(substr),
+                )
+            )
+
+        if birth_date:
+            query = query.where(self.model.birth_date == birth_date)
+        result: AsyncResult = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_by_login(self, login: str) -> User:
+        query = select(self.model).where(
+            self.model.deleted_at.is_(None),
+            self.model.credentials.has(Credential.login == login),
+        )
+        result: AsyncResult = await self.session.execute(query)
+        return result.scalars().one()
+
+
+class CredentialRepository(BaseSqlAlchemyRepository):
+    model = Credential
+
+
+class RefreshTokenRepository(BaseSqlAlchemyRepository):
+    model = RefreshToken
