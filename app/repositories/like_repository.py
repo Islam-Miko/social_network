2901652b9c0c2da46014a1e50991387dec8c0269
miko <islamimanhod@gmail.com>
2023-03-30 22:17:54 +0600

    fix: refactor LikeRepostiry

--- app/repositories/like_repository.py
+++ app/repositories/like_repository.py
@@ -1,5 +1,4 @@
 from sqlalchemy import delete, insert
-from sqlalchemy.ext.asyncio import AsyncResult
 
 from app.models.post_model import Like
 from app.repositories.base_repository import BaseSqlAlchemyRepository
@@ -8,15 +7,10 @@ from app.repositories.base_repository import BaseSqlAlchemyRepository
 class LikeRepository(BaseSqlAlchemyRepository):
     model = Like
 
-    async def like_post(self, post_id: int, user_id: int) -> int:
-        stmt = (
-            insert(Like)
-            .values(user_id=user_id, post_id=post_id)
-            .returning(Like.id)
-        )
-        result: AsyncResult = await self.session.execute(stmt)
+    async def like_post(self, post_id: int, user_id: int) -> None:
+        stmt = insert(Like).values(user_id=user_id, post_id=post_id)
+        await self.session.execute(stmt)
         await self.session.commit()
-        return result.scalar_one()
 
     async def dislike_post(
         self,
