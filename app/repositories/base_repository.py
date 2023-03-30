94a8dbe1b2d495a9b2f685b5a879a2109404ac84
miko <islamimanhod@gmail.com>
2023-03-30 21:37:00 +0600

    fix: change create methods implemantation, enable orm_mode to schemas

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -2,9 +2,8 @@ from collections.abc import Iterable, Mapping, Sequence
 from typing import Any, ClassVar, TypeVar, Union
 
 from fastapi import Depends
-from sqlalchemy import delete, update
+from sqlalchemy import Select, delete, func, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncResult, AsyncSession
-from sqlalchemy.future import select
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql import exists as origin_exists
 
@@ -21,19 +20,20 @@ class BaseSqlAlchemyRepository:
 
     session: AsyncSession
     model: ClassVar
+    base_select: Select
 
     def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
         self.session = session
 
-    async def create(self, instance: Model) -> Model:
-        self.session.add(instance)
-        await self.session.flush()
+    async def create(self, data: Mapping) -> Model:
+        query = insert(self.model).values(**data).returning(self.model.id)
+        result: AsyncResult = await self.session.execute(query)
         await self.session.commit()
-        return instance
+        return result.scalar_one()
 
     async def get(self, id: Key) -> Model:
         try:
-            query = select(self.model).where(id == id)
+            query = self.base_select.where(self.model.id == id)
             result: AsyncResult = await self.session.execute(query)
             return result.scalars().one()
         except NoResultFound:
@@ -49,14 +49,14 @@ class BaseSqlAlchemyRepository:
             )
             result: AsyncResult = await self.session.execute(query)
             await self.session.commit()
-            return result.first()
+            return result.scalar_one()
         except NoResultFound:
             raise
 
     async def all(
         self, limit: int, offset: int, *args: Sequence
     ) -> list[Model]:
-        query = select(self.model).filter(self.model.deleted_at.is_(None))  # type: ignore
+        query = self.base_select  # type: ignore
         if args:
             query.filter(*args)
         query = query.limit(limit).offset(offset)
@@ -69,7 +69,7 @@ class BaseSqlAlchemyRepository:
         return result.scalar_one()
 
     async def get_or_none(self, *args: Iterable) -> Union[None, Model]:
-        query = select(self.model).filter(*args)
+        query = self.base_select.where(*args)
         result: AsyncResult = await self.session.execute(query)
         return result.scalars().first()
 
@@ -77,3 +77,13 @@ class BaseSqlAlchemyRepository:
         query = delete(self.model).where(self.model.id == id)
         await self.session.execute(query)
         await self.session.commit()
+
+    async def count(self, *args) -> int:
+        query = (
+            select(func.count())
+            .select_from(self.model)
+            .where(self.model.deleted_at.is_(None))
+            .where(*args)
+        )
+        result: AsyncResult = await self.session.execute(query)
+        return result.scalar_one()
