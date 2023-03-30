aaf3b0fa4ac28666ff186e9aa976abf06bcb05e9
miko <islamimanhod@gmail.com>
2023-03-30 21:37:48 +0600

    fix: add base_select to post and user repos

--- app/repositories/post_repository.py
+++ app/repositories/post_repository.py
@@ -1,6 +1,9 @@
+from sqlalchemy import select
+
 from app.models.post_model import Post
 from app.repositories.base_repository import BaseSqlAlchemyRepository
 
 
 class PostRepository(BaseSqlAlchemyRepository):
     model = Post
+    base_select = select(Post).where(Post.deleted_at.is_(None))
